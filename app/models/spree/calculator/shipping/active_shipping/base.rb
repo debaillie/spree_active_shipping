ede787ceff6be62503be037d1af9705ea0de2e0a
Tim DeBaillie <tim.debaillie@ciholas.com>
2015-06-10 10:39:18 -0500

    Changed default shipping option to pick-up, not dropoff.

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -96,7 +96,7 @@ module Spree
 
         def retrieve_rates(origin, destination, shipment_packages)
           begin
-            response = carrier.find_rates(origin, destination, shipment_packages)
+            response = carrier.find_rates(origin, destination, shipment_packages, :dropoff_type => 'REQUEST_COURIER')
             # turn this beastly array into a nice little hash
             rates = response.rates.collect do |rate|
               service_name = rate.service_name.encode("UTF-8")
